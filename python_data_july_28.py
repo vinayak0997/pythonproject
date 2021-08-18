ea8bec783b8c483e5f8f6dd3829279d1a32b1c3c
charveechoubisa <89144498+charveechoubisa@users.noreply.github.com>
2021-08-18 19:26:44 +0530

    Add files via upload
    trial commit changes

---
+++ python_data_july_28.py
@@ -0,0 +1,57 @@
+##
+##my_num =2.0
+##print (my_num)
+##print(type(my_num))
+##
+##
+##
+##my_string ="charvee"
+##my_string[3]="e"
+##print(my_string)
+##my_num =2.0
+##print (my_num)
+##print(type(my_num))
+##
+##
+##
+
+
+
+##my_string ="charvee"
+##my_string[3]="e"
+##print(my_string)
+##
+##
+##my_path = r"c:\newfolder\time.txt"
+##print(my_path)
+##
+##
+##
+##my_string = input("enter ur name")
+##print(my_string)
+##print(type(my_string))
+##
+##
+##my_string =int(input("enter your number:"))
+##print(my_string)
+##print(type(my_string))
+
+
+##my_string ="['charvee' jungkook']"
+##print(my_string)
+##print(type(my_string))
+
+##my_num=3.334
+##print(my_num)
+##print(type(my_num))
+
+
+
+
+##my_string ='''I love my country \ 
+##and I live in INdia'''
+##print(my_string)
+
+my_num1 =5
+my_num2 =3
+print(my_num1+my_num2)
